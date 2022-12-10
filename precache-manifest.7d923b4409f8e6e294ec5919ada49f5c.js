4c26ca45c4956e6a7210a18732b3419065e0474e
Alejandra J. Perea Rojas <aperearojas@student.harvard.edu>
2022-12-10 17:43:45 -0500

    Updates

---
+++ precache-manifest.7d923b4409f8e6e294ec5919ada49f5c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c01d4afdbc5303f675d",
+    "url": "/static/css/main.6d8c1ad8.chunk.css"
+  },
+  {
+    "revision": "1c01d4afdbc5303f675d",
+    "url": "/static/js/main.1c01d4af.chunk.js"
+  },
+  {
+    "revision": "3c916afe3858d0874a09",
+    "url": "/static/js/1.3c916afe.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6b8a4e80b1f3496927e351006d0ac55",
+    "url": "/static/media/github.d6b8a4e8.svg"
+  },
+  {
+    "revision": "839ba28e5ec6bcec6fb9d96666b15ad8",
+    "url": "/static/media/linkedin.839ba28e.svg"
+  },
+  {
+    "revision": "4f824e5f2f99dfc63522b069492b8dbf",
+    "url": "/static/media/IMG_8171.4f824e5f.JPG"
+  },
+  {
+    "revision": "44cc5f0d56fc18563ede540087b21280",
+    "url": "/static/media/cv.44cc5f0d.pdf"
+  },
+  {
+    "revision": "0a1e4c9873d2693f1223bd910d553e7d",
+    "url": "/static/media/hufpi.0a1e4c98.png"
+  },
+  {
+    "revision": "4646e74a9be88ed00d644f70a0de3140",
+    "url": "/static/media/PeruHMUN.4646e74a.png"
+  },
+  {
+    "revision": "c89dd03163e99387a3cf7fd2c737dbd3",
+    "url": "/static/media/interface.c89dd031.png"
+  },
+  {
+    "revision": "2aedafde3d10155e36207fa60abe4767",
+    "url": "/static/media/yspa.2aedafde.png"
+  },
+  {
+    "revision": "fa5cc74f990c26ec7ed22cb7a6b52456",
+    "url": "/static/media/PereaRojas-PeruPolicyProject.fa5cc74f.pdf"
+  },
+  {
+    "revision": "0d6bd86296313b672f1b24bf8dbf7f3e",
+    "url": "/static/media/PERUMUN2020-LEGAL.0d6bd862.pdf"
+  },
+  {
+    "revision": "81ac8826f307e4ee24e3d70105cc0b53",
+    "url": "/static/media/PereaRojas-WebBasedInterface.81ac8826.pdf"
+  },
+  {
+    "revision": "d7faac6130d1557ba358b4f903a3bf84",
+    "url": "/static/media/Team4-YSPA.d7faac61.pdf"
+  },
+  {
+    "revision": "cb84583ece078d1512466550ad40ae9a",
+    "url": "/index.html"
+  }
+];
